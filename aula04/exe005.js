a8c6cfd62ff0fb7b3e06819bce5afbc7e837b8a1
RNumata <regis.numata@gmail.com>
2022-12-29 20:54:50 -0300

    Exercicio JS

--- aula04/exe005.js
+++ aula04/exe005.js
@@ -1,2 +1,25 @@
-let n = 1;
-console.log(++n); //f8 para ativar node
+
+function carregar(){
+  let msg = document.getElementById('msg');
+  let img = document.getElementById('imagem');
+
+  let data = new Date();
+  let hora = data.getHours();
+
+  msg.innerHTML = `Agora são ${hora} horas`;
+
+  if(hora >= 0 && hora < 12){
+    img.src = 'manha.jpg';
+    document.body.style.background = '#e2cd9f';
+  }else if (hora >= 12 && hora <= 18){
+    img.src = 'tarde.jpg';
+    document.body.style.background = '#b9846f'
+  }else{
+    img.src = 'noite.jpg';
+    document.body.style.background = '#515154';
+  }
+
+}
+
+
+
