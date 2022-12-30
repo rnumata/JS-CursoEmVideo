446c16a8a090755043b1e7cf6c4c0c78b7df35c4
RNumata <regis.numata@gmail.com>
2022-12-30 15:09:45 -0300

    Repeticao

---
+++ aula04/exe007.js
@@ -0,0 +1,13 @@
+//F8 abre o node
+let contador = 1;
+while (contador < 5) {
+  console.log(`while => ${contador}`);
+  contador++;
+}
+
+let c = 1;
+do {
+  console.log(`do => ${c}`);
+  c++;
+} while (c <= 5);
+
